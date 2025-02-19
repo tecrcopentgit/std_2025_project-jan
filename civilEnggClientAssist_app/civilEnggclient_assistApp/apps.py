5f130e114e7ea131fad2bd554a4302b1e6d379c9
tecrcopentgit <abdulhaadhi015@gmail.com>
2025-02-19 22:45:10 +0530

    django implemented and initiated

---
+++ civilEnggClientAssist_app/civilEnggclient_assistApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CivilenggclientAssistappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'civilEnggclient_assistApp'
