4d7d25f95eaacba1b87a135fabc9a29b9a3a33d0
tecrcopentgit <abdulhaadhi015@gmail.com>
2025-02-19 22:54:40 +0530

    model.py content deleted

--- civilEnggClientAssist_app/civilEnggclient_assistApp/models.py
+++ civilEnggClientAssist_app/civilEnggclient_assistApp/models.py
@@ -1,17 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-class Client(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    contact_number = models.CharField(max_length=15)
-    address = models.TextField()
-
-class Organizer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    position = models.CharField(max_length=100)
-
-class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
