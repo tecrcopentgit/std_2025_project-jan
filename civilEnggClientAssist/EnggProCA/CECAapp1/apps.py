b6539d8b313d49fce9872421093f1e84796183c9
tecrcopentgit <abdulhaadhi015@gmail.com>
2025-02-20 17:52:54 +0530

    django section updated and html,css,js directories are changed ,And django worked

--- civilEnggClientAssist/EnggProCA/CECAapp1/apps.py
+++ civilEnggClientAssist/EnggProCA/CECAapp1/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CivilenggclientAssistappConfig(AppConfig):
+class Cecaapp1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'civilEnggclient_assistApp'
+    name = 'CECAapp1'
