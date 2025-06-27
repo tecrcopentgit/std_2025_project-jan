3f7ab296c52ec009da017879b620bdab599c19a3
Abdul Haadhi <abdulhaadhi015@gmail.com>
2025-06-27 21:25:31 +0530

    Delete CONTROL directory

--- CONTROL/Client/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ProjectProposal
-
-class ProjectProposalForm(forms.ModelForm):
-    class Meta:
-        model = ProjectProposal
-        fields = ['name', 'phone', 'user_address', 'land_location']
